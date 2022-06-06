41f89d11aec9d293830c64dbdce4b5635e271813
kellyquintesa <kellyquintesa11@gmail.com>
2022-06-06 16:10:53 +0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div id="Header" class="container">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
